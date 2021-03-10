199087cca098ce90320e76988227695d76b84f9b
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-03-10 18:10:12 -0300

    chore: listing grocery list with ng-repeat

--- grocery-list-app/js/app.js
+++ grocery-list-app/js/app.js
@@ -9,7 +9,7 @@ app.controller("GroceryListItemsController", ["$scope", function($scope){
         {itemName: 'milk', date: '2021-03-08'},
         {itemName: 'cookies', date: '2021-03-08'},
         {itemName: 'ice cream', date: '2021-03-08'},
-        {itemName: 'potatoes', date: '2021-03-09},
+        {itemName: 'potatoes', date: '2021-03-09'},
         {itemName: 'cereal', date: '2021-03-09'},
         {itemName: 'bread', date: '2021-03-09'},
         {itemName: 'eggs', date: '2021-03-10'},
