8cf203a258cab8c3a660dfc879cd9dd613f077ca
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-03-12 13:51:26 -0300

    feat:  routing through pages

--- grocery-list-app/js/app.js
+++ grocery-list-app/js/app.js
@@ -1,19 +1,38 @@
+/**
+ * Created by Thomas on 5/28/2015.
+ */
 var app = angular.module('groceryListApp', ["ngRoute"]);
+
+app.config(function($routeProvider){
+    $routeProvider
+        .when("/",{
+            templateUrl: "views/groceryList.html",
+            controller: "GroceryListItemsController"
+        })
+        .when("/addItem",{
+            templateUrl: "views/addItem.html",
+            controller: "GroceryListItemsController"
+        })
+        .otherwise({
+            redirectTo: "/"
+        })
+});
 
 app.controller("HomeController", ["$scope", function($scope) {
     $scope.appTitle = "Grocery List";
 }]);
 
 app.controller("GroceryListItemsController", ["$scope", function($scope){
+
     $scope.groceryItems = [
-        {itemName: 'milk', date: '2021-03-08'},
-        {itemName: 'cookies', date: '2021-03-08'},
-        {itemName: 'ice cream', date: '2021-03-08'},
-        {itemName: 'potatoes', date: '2021-03-09'},
-        {itemName: 'cereal', date: '2021-03-09'},
-        {itemName: 'bread', date: '2021-03-09'},
-        {itemName: 'eggs', date: '2021-03-10'},
-        {itemName: 'tortillas', date: '2021-03-10'}
+        {completed: true, itemName: 'milk', date: '2014-10-00'},
+        {completed: true, itemName: 'cookies', date: '2014-10-01'},
+        {completed: true, itemName: 'ice cream', date: '2014-10-02'},
+        {completed: true, itemName: 'potatoes', date: '2014-10-02'},
+        {completed: true, itemName: 'cereal', date: '2014-10-03'},
+        {completed: true, itemName: 'bread', date: '2014-10-03'},
+        {completed: true, itemName: 'eggs', date: '2014-10-04'},
+        {completed: true, itemName: 'tortillas', date: '2014-10-04'}
     ]
 
-}])
+}]);
