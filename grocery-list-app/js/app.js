b12e54f1f2d7239eb5146b632e417e62c3350b22
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-04-05 09:58:06 -0300

    chore: app conclusion and http services

--- grocery-list-app/js/app.js
+++ grocery-list-app/js/app.js
@@ -1,50 +1,128 @@
+/**
+ * Created by Thomas on 5/28/2015.
+ */
 var app = angular.module('groceryListApp', ["ngRoute"]);
 
 app.config(function($routeProvider){
     $routeProvider
         .when("/",{
             templateUrl: "views/groceryList.html",
-            controller: "GroceryListItemsController"
+            controller: "HomeController"
         })
         .when("/addItem",{
             templateUrl: "views/addItem.html",
-            controller: "GroceryListItemsController"
+            controller: "GroceryListItemController"
         })
-        //passing parameters
-        .when("/addItem/:id",{
-            templateUrl: "views/addItem.html",
-            controller: "GroceryListItemsController"
+        .when('/addItem/edit/:id',{
+            templateUrl: 'views/addItem.html',
+            controller: 'GroceryListItemController'
         })
         .otherwise({
             redirectTo: "/"
         })
 });
 
-app.controller("HomeController", "GroceryService", ["$scope", "GroceryService", function($scope) {
-    $scope.appTitle = "Grocery List";
-}]);
-
 app.service("GroceryService", function(){
 
     var groceryService = {};
 
     groceryService.groceryItems = [
-        {completed: true, itemName: 'milk', date: '2014-10-00'},
-        {completed: true, itemName: 'cookies', date: '2014-10-01'},
-        {completed: true, itemName: 'ice cream', date: '2014-10-02'},
-        {completed: true, itemName: 'potatoes', date: '2014-10-02'},
-        {completed: true, itemName: 'cereal', date: '2014-10-03'},
-        {completed: true, itemName: 'bread', date: '2014-10-03'},
-        {completed: true, itemName: 'eggs', date: '2014-10-04'},
-        {completed: true, itemName: 'tortillas', date: '2014-10-04'}
-    ]
+        {id: 1, completed: false, itemName: 'milk', date: new Date("February 21, 2021 12:45:35")},
+        {id: 2, completed: false, itemName: 'cookies', date: new Date("February 21, 2021 12:45:35")},
+        {id: 3, completed: false, itemName: 'ice cream', date: new Date("February 22, 2021 12:45:35")},
+        {id: 4, completed: false, itemName: 'potatoes', date: new Date("February 22, 2021 12:45:35")},
+        {id: 5, completed: false, itemName: 'cereal', date: new Date("February 23, 2021 12:45:35")},
+        {id: 6, completed: false, itemName: 'bread', date: new Date("February 23, 2021 12:45:35")},
+        {id: 7, completed: false, itemName: 'eggs', date: new Date("February 23, 2021 12:45:35")},
+        {id: 8, completed: false, itemName: 'tortillas', date: new Date("February 25, 2021 12:45:35")}
+    ];
+
+
+    groceryService.findById = function(id){
+        for(var item in groceryService.groceryItems){
+            if(groceryService.groceryItems[item].id === id) {
+                console.log(groceryService.groceryItems[item]);
+                return groceryService.groceryItems[item];
+            }
+        }
+    };
+
+    groceryService.getNewId = function(){
+
+        if(groceryService.newId){
+            groceryService.newId++;
+            return groceryService.newId;
+        }else{
+            var maxId = _.max(groceryService.groceryItems, function(entry){ return entry.id;})
+            groceryService.newId = maxId.id + 1;
+            return groceryService.newId;
+        }
+    };
+
+    groceryService.markCompleted = function(entry){
+        entry.completed = !entry.completed;
+    };
+
+    groceryService.removeItem = function(entry){
+        var index = groceryService.groceryItems.indexOf(entry);
+
+        groceryService.groceryItems.splice(index, 1);
+    };
+
+    groceryService.save = function(entry) {
+
+        var updatedItem = groceryService.findById(entry.id);
+
+        if(updatedItem){
+
+            updatedItem.completed = entry.completed;
+            updatedItem.itemName = entry.itemName;
+            updatedItem.date = entry.date;
+
+        }else {
+            entry.id = groceryService.getNewId();
+            groceryService.groceryItems.push(entry);
+        }
+
+    };
 
     return groceryService;
-})
 
-app.controller("GroceryListItemsController", ["$scope",  "$routeParams", "GroceryService", function($scope, $routeParams, GroceryService){
+});
+
+app.controller("HomeController", ["$scope", "GroceryService", function($scope, GroceryService) {
 
     $scope.groceryItems = GroceryService.groceryItems;
 
-    $scope.rp = "Route Parameter value: " + $routeParams.id;
+    $scope.removeItem = function(entry){
+        GroceryService.removeItem(entry);
+    };
+
+    $scope.markCompleted = function(entry){
+        GroceryService.markCompleted(entry);
+    };
+
 }]);
+
+app.controller("GroceryListItemController", ["$scope", "$routeParams", "$location", "GroceryService", function($scope, $routeParams, $location, GroceryService){
+
+    if(!$routeParams.id) {
+        $scope.groceryItem = {id: 0, completed: false, itemName: "", date: new Date()};
+    }else{
+        $scope.groceryItem = _.clone(GroceryService.findById(parseInt($routeParams.id)));
+    }
+
+
+    $scope.save = function(){
+        GroceryService.save( $scope.groceryItem );
+        $location.path("/");
+    };
+
+}]);
+
+app.directive("tbGroceryItem", function(){
+    return{
+        restrict: "E",
+        templateUrl: "views/groceryItem.html"
+    }
+});
